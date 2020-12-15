a35ee952409f3d0d4edc90c7cabc1b4db0ae6404
Winterworks <mail@bramwinter.nl>
2020-12-15 15:01:30 +0100

    Add first solution for day fifteen

---
+++ solutions/day15.py
@@ -0,0 +1,25 @@
+data_file = open('./data/data15')
+numbers = list(map(lambda x: int(x), data_file.read().split(',')))
+data_file.close()
+
+def say_next(ages, last_number, i):
+    new_number = 0
+    if ages[last_number]['occ'] > 1:
+        new_number = i - ages[last_number]['index']
+        ages[last_number]['index'] = i
+    if new_number in ages:
+        ages[new_number]['occ'] += 1
+    else:
+        ages[new_number] = {'index': i+1, 'occ': 1}
+    return new_number
+
+def get_nth_number(numbers, n):
+    ages = {}
+    for i, number in enumerate(numbers):
+        ages[number] = {'index': i+1, 'occ': 1}
+    last_number = numbers[-1]
+    for i in range(len(numbers), n):
+        last_number = say_next(ages, last_number, i)
+    return last_number
+
+print(get_nth_number(numbers, 2020))
